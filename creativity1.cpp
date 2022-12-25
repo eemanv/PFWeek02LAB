f665d20d223882b8076ca395e1570aa857c5deff
eemanv <eemanmuzammil301@gmail.com>
2022-12-25 21:49:55 +0500

    Work

---
+++ creativity1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout << "                           *        *                          " << endl;
+cout << "                     *                   *                     " << endl;
+cout << "                  * @                     @  *                  " << endl;
+cout << "                *  @   @               @   @   *                " << endl;
+cout << "              *    @      @         @      @     *              " << endl;
+cout << "             *     @         @   @         @      *             " << endl;
+cout << "            *      @  CCCC   @   @   PPPP  @       *            " << endl;
+cout << "           *       @ C       @   @  P    P @       *           " << endl;
+cout << "           *       @ C       @   @  P    P @       *           " << endl;
+cout << "           *       @ C       @   @  PPPPP  @       *           " << endl;
+cout << "           *       @ C       @   @  P      @       *           " << endl;
+cout << "            *      @  CCCC   @   @  P      @      *            " << endl;
+cout << "             *     @         @   @         @     *             " << endl;
+cout << "               *      @      @   @      @      *               " << endl;
+cout << "                 *        @  @   @  @        *                   " << endl;
+cout << "                     *       @   @       *                    " << endl;
+cout << "                           *       *                  " << endl;
+cout << "                                                   " << endl;
+}
---
+++ example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout << "My Name is Eeman";
+cout << endl;
+cout << "My Roll Number is 12" << endl << "My age is 17" << endl;
+cout << "My bestfriend is ZAINAB IDREES" << endl;
+cout << "My other bestfriend is DANIA SAQIB";
+}
